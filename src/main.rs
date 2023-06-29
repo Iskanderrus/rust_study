b00590b2932491d7aacf633204b0991586882b5c
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-06-29 13:15:35 +0300

    comments added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// This is the entry point of 
+// the application
 fn main() {
+    // Display a message to the user.
     println!("Hello, world!");
+    
+    let my_favourite_color: &str = "red";
 }
