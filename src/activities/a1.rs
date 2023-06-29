39669ec53f6b8f137df11c070d3dc6650ad27861
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-06-29 13:24:53 +0300

    cleanup

--- src/activities/a1.rs
+++ src/activities/a1.rs
@@ -1,5 +1,5 @@
-    let first_name = "Alexander"; 
-    let last_name = "Chasovskoy"; 
+let first_name = "Alexander"; 
+let last_name = "Chasovskoy"; 
 
 fn main() {
     println!({:?} {:?}, first_name, last_name);
