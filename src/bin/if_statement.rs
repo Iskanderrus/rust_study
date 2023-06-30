484de97dfa3e040fdc6884bb4e3d9cdbff071cee
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-06-30 15:03:09 +0300

    if statement with "and logic"

--- src/bin/if_statement.rs
+++ src/bin/if_statement.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let age = 91; 
-    if age > 90 {
-        println!("it's too late to drink a beer");
-    } else if age >= 21 {
+    let age = 13; 
+    if age >= 21 && age < 90 {
         println!("ok to purchase a beer");
-    } else {
+    } else if age <= 21 {
         println!("you are too young to buy a beer");
+    } else {
+        println!("it's too late to drink a beer");
     }
 }
