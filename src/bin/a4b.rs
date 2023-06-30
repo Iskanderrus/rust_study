29e0883cd92a233952130494d9524d711f41e5eb
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-06-30 15:32:06 +0300

    match flow control with multiple choices

---
+++ src/bin/a4b.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_num = 3; 
+    match my_num {
+        1 => println!("one"),
+        2 => println!("two"), 
+        3 => println!("three"), 
+        _ => println!("other"),  
+    }
+}
