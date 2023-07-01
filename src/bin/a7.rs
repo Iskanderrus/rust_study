007f08fcd09b7b6d34f14d1c90d35d294effeddd
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-07-01 23:51:08 +0300

    colors enumeration

---
+++ src/bin/a7.rs
@@ -0,0 +1,24 @@
+enum Colors {
+    White, 
+    Black, 
+    Blue,
+    Green, 
+    Yellow, 
+    Red, 
+    Orange,
+    Purple, 
+}
+
+fn main() {
+    let my_car_color = Colors::Blue; 
+    match my_car_color {
+        Colors::Black => println!("My car is black"),
+        Colors::Blue => println!("My car color is blue"), 
+        Colors::Red => println!("My car color is red"), 
+        Colors::White => println!("My car color is white"), 
+        Colors::Orange => println!("My car color is orange"), 
+        Colors::Green => println!("My car color is green"),
+        Colors::Purple => println!("My car color is purple"),
+        Colors::Yellow => println!("My car color is yellow")
+    }
+}
