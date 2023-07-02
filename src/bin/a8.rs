93f1f53559fb336ef131820550419c6c9f2053bf
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-07-02 12:48:01 +0300

    formatting printing function

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -16,32 +16,32 @@ struct Drink {
 fn product_printing(drink: Drink) {
 
     match drink.brand {
-        Brand::Dobry => println!("Drink brand is: Dobry"),
+        Brand::Dobry => println!("Drink brand is: \tDobry"),
     };
      
     match drink.flavor {
-        DrinkFlavor::Apple => println!("Drink flavor is: Apple"),
-        DrinkFlavor::Orange => println!("Drink flavor is: Orange"), 
-        DrinkFlavor::Vishne => println!("Drink flavor is: Vishne"), 
+        DrinkFlavor::Apple => println!("Drink flavor is: \tApple"),
+        DrinkFlavor::Orange => println!("Drink flavor is: \tOrange"), 
+        DrinkFlavor::Vishne => println!("Drink flavor is: \tVishne"), 
     };
-    println!("Drink volume: {:?} FLOZ\n", drink.volume); 
+    println!("Drink volume: \t\t{:?} FLOZ\n", drink.volume); 
 }
 
 fn main() {
     let sok_dobry_apple = Drink {
         brand: Brand::Dobry,
         flavor: DrinkFlavor::Apple, 
-        volume: 2.5,
+        volume: 12.5,
     };
     let sok_dobry_orange = Drink {
         brand: Brand::Dobry,
         flavor: DrinkFlavor::Orange, 
-        volume: 2.5,
+        volume: 7.5,
     };
     let sok_dobry_vishne = Drink {
         brand: Brand::Dobry,       
         flavor: DrinkFlavor::Vishne, 
-        volume: 2.5,
+        volume: 6.0,
     };
     product_printing(sok_dobry_apple);
     product_printing(sok_dobry_orange);
